d7a9a7bdaaa85ae11c740fcbcfedb733048c8eeb
jothebanner <hawks.jacob.g@gmail.com>
2022-12-21 10:25:46 -0700

    Used certbot to get the privkey and certification. Added secure files to gitignore. Installed basicsSsl for self-signed testing; leaving it for future use. modified vite.config to use the certbot supplied ssl data.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+import * as fs from 'fs'
+import * as path from 'path'
+
+let pk = fs.readFileSync(path.join(__dirname, './https/privkey.pem'));
+let cert = fs.readFileSync(path.join(__dirname, './https/cert.pem'));
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()]
+export default ({mode}) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  return defineConfig({
+  plugins: [
+    svelte(),
+    //basicSsl(),
+  ],
+  server: {port: 4002,
+          https:{key: pk, cert: cert,}}
 })
+}
